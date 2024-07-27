a352da2405c4c15747876a033160503797e6311f
TusharMinche <tusharminche@gmail.com>
2024-07-27 19:23:42 +0530

    write-read-from-file folder updated

--- write-read-from-file/app.js
+++ write-read-from-file/app.js
@@ -1,39 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-
-    if(req.url === '/'){
-        res.write('<html>');
-        res.write('<title>My page</title>');
-        res.write('<body>');
-        const msg = fs.readFileSync('./msg.txt');
-        res.write(msg);
-        res.write('<form action="/msg" method="POST"><input type="text" name="msg"/><button type="submit">send</button></form>');
-        res.write('</body>');
-        res.write('</html>');
-        res.end();
-    }
-
-    if(req.url === '/msg' && req.method === 'POST'){
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-
-            fs.writeFileSync('msg.txt', message);
-
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            
-            return res.end();
-        })
-        return;
-    }
-});
+const server = http.createServer(routes);
 
 server.listen(4000);
