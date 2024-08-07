3b3f2ca20cdd80f345fba011a08972ff4b2617d6
TusharMinche <tusharminche@gmail.com>
2024-08-07 15:08:49 +0530

    File sysytem added

--- express/Middlewares/controllers/admin.js
+++ express/Middlewares/controllers/admin.js
@@ -1,11 +1,17 @@
 const rootDir = require('../util/path');
 const path = require('path');
 
+const Product = require('../models/product');
+
 exports.addProduct = (req, res) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 }
 
 exports.shop = (req, res) => {
-    console.log(req.body);
+    const product = new Product(req.body.title);
+    product.save();
+
+    console.log(product.fetchAll);
+
     res.redirect('/shop');
 }
